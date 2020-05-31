8b008a003d32d4aeacbfefcff13046e52b8632ff
Deepak Uniyal <consummatelydone@gmail.com>
2020-05-31 17:52:34 +0530

    final changes pagination, subscription

---
+++ README.md
@@ -0,0 +1,12 @@
+Tutorial followed from https://www.howtographql.com/react-apollo
+
+Links,
+Create Links,
+Queries : Loading Links,
+Mutations : Creating Links,
+Routing,
+Authentication,
+Filtering,
+Subscription,
+Pagination
+
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { withRouter } from 'react-router'
 import { AUTH_TOKEN } from '../constants'
-
 class Header extends Component {
     render() {
         const authToken = localStorage.getItem(AUTH_TOKEN)
@@ -10,6 +9,12 @@ class Header extends Component {
           <div className="flex pa1 justify-between nowrap orange">
             <div className="flex flex-fixed black">
               <div className="fw7 mr1">Hacker News</div>
+              
+              <Link to="/top" className="ml1 no-underline black">
+                top
+              </Link>
+              <div className="ml1">|</div>
+
               <Link to="/" className="ml1 no-underline black">
                 new
               </Link>
